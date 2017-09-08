dd8ac8bc67239a6a568590d76a4db477b9453158
nickedwards <nick@nick-edwards.co.uk>
2017-09-08 17:06:53 +0100

    testing stuff out

--- index.js
+++ index.js
@@ -4,23 +4,28 @@ var Alexa = require('alexa-sdk');
 const APP_ID = "amzn1.ask.skill.8e774613-8603-4e15-97eb-95efdfcf3caf";
 
 const SKILL_NAME = "Learn Times Tables";
-const HELP_MESSAGE = "You can ask me to read a number's times table, for example try asking for the 5 times table up to 7";
+const HELP_MESSAGE = "You can ask me to read a number's times table, for example try asking for the 5 times table up to 7. Or, you can ask to be quizzed, for example try asking quiz me on the 8 times table.";
 const CANCEL_MESSAGE = "Would you like to hear another times table?";
 const STOP_MESSAGE = "Goodbye!";
 const HELP_REPROMPT = "Which times table would you like to hear?";
 const UNKNOWN_RESPONSE = "Sorry, I didn't understand that.";
 const PAUSE = "<break time=\"1s\"/>";
 
+var states = {
+    TIMESTABLE: "_TIMESTABLE",
+    QUIZ: "_QUIZ"
+};
+
 // This is the function that AWS Lambda calls every time Alexa uses your skill.
 exports.handler = function(event, context, callback) {
     // Create an instance of the Alexa library and pass it the requested command.
     var alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
-    alexa.registerHandlers(handlers);
+    alexa.registerHandlers(timesTableHandlers);
     alexa.execute();
 };
 
-var handlers = {
+var timesTableHandlers = Alexa.CreateStateHandler(states.TIMESTABLE, {
     'LaunchRequest': function () {
       this.response.speak('Welcome to ' + SKILL_NAME + '.' + PAUSE + HELP_REPROMPT).listen(HELP_REPROMPT);
       this.emit(':responseReady');
@@ -28,10 +33,10 @@ var handlers = {
 
     'TimesTableIntent': function () {
         // set a default maxNumber
-        var maxNumber = 10;
-        // get variables from input slots
-        var inputNumber = parseInt(this.event.request.intent.slots.number.value);
-        var inputMax = parseInt(this.event.request.intent.slots.inputMax.value);
+        var maxNumber = 10,
+            // get variables from input slots
+            inputNumber = parseInt(this.event.request.intent.slots.number.value),
+            inputMax = parseInt(this.event.request.intent.slots.inputMax.value);
 
         if (isNaN(inputNumber)) {
             errorMessage = 'Sorry, I didn\'t understand that number. Please ask for the times table you would like to hear. For example <emphasis>the 3 times table</emphasis>.';
@@ -78,4 +83,78 @@ var handlers = {
         this.response.speak(UNKNOWN_RESPONSE + PAUSE + HELP_REPROMPT).listen(HELP_REPROMPT);
         this.emit(':responseReady');
     }
-};
+});
+
+var timesTableHandlers = Alexa.CreateStateHandler(states.TIMESTABLE, {
+    'QuizStartIntent': function () {
+        // define defaults
+        this.attributes["counter"] = 0;
+        this.attributes["quizscore"] = 0;
+        var numberOfQuestions = 5,
+            // get variables from input slots
+            inputNumberOfQuestions = parseInt(this.event.request.intent.slots.numberOfQuestions.value),
+            inputNumberToUse = parseInt(this.event.request.intent.slots.numberToUse.value);
+
+        if (!isNaN(inputNumberOfQuestions)) {
+            numberOfQuestions = inputNumberOfQuestions;
+        }
+
+        // generate questions
+        var questions = new Object();
+        for(var i=1; i <= numberOfQuestions; i++){
+            if (isNaN(inputNumberToUse)) {
+                // generate a random number between 1 and 10
+                inputNumberToUse = getRandomInt(1,10);
+            }
+            // generate a random number between 1 and 10
+            var numberToMultiplyBy = getRandomInt(1,10);
+            var sumEquals = inputNumberToUse * numberToMultiplyBy;
+            var question = "What is " + inputNumberToUse " times " + numberToMultiplyBy;
+            questions[i] =  new Object();
+            questions[i][question] = sumEquals;
+        }
+
+        // save questions in session
+        
+        // ask first question
+
+    }
+
+    'QuizNextQuestionIntent': function () {
+
+    }
+
+    'QuizAnswerIntent': function () {
+
+    }
+
+    'AMAZON.HelpIntent': function () {
+        this.response.speak(HELP_MESSAGE + PAUSE + HELP_REPROMPT).listen(HELP_REPROMPT);
+        this.emit(':responseReady');
+    },
+
+    'AMAZON.CancelIntent': function () {
+        this.response.speak(CANCEL_MESSAGE + PAUSE + HELP_REPROMPT).listen(HELP_REPROMPT);
+        this.emit(':responseReady');
+    },
+
+    'AMAZON.StopIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+
+    'ErrorHandler': function() {
+        this.response.speak(errorMessage + PAUSE + HELP_REPROMPT).listen(HELP_REPROMPT);
+        this.emit(':responseReady');
+    },
+
+    'Unhandled': function() {
+        this.response.speak(UNKNOWN_RESPONSE + PAUSE + HELP_REPROMPT).listen(HELP_REPROMPT);
+        this.emit(':responseReady');
+    }
+});
+
+function getRandomInt(min, max)
+{
+    return Math.floor(Math.random() * (max-min+1)+min);
+}
